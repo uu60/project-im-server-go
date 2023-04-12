29aa3303bd33080d79d10b49b3911e4468bfd934
uu60 <djz6660@icloud.com>
2023-04-12 22:09:53 +0800

    v0.2 用户上线及广播功能

--- main/server.go
+++ main/server.go
@@ -3,16 +3,29 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+	// 在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	// 创建一个基本的对象
-	server := Server{Ip: ip, Port: port}
+	server := Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		mapLock:   sync.RWMutex{},
+		Message:   make(chan string),
+	}
 	return &server
 }
 
@@ -25,6 +38,9 @@ func (this *Server) Start() {
 	}
 	// close listener socket finally
 	defer listen.Close()
+
+	// 启动监听Message
+	go this.ListenMessage()
 
 	// loop accept
 	for {
@@ -43,5 +59,31 @@ func (this *Server) Start() {
 
 func (this *Server) Handle(conn net.Conn) {
 	// 处理当前连接请求
-	fmt.Println("连接建立成功")
+	//fmt.Println("连接建立成功")
+	user := NewUser(conn)
+
+	// 用户上线，将用户加入到onlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	this.BroadCast(user, "已上线")
+}
+
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ": " + msg
+	this.Message <- sendMsg
+}
+
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
+
+		// 将msg发送给全部在线的user
+		this.mapLock.RLock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.RUnlock()
+	}
 }
